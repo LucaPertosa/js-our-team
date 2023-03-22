2357a272c6cacf988816336cc77b6e3b726ace0c
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-22 17:19:31 +0100

    Milestone 0: creare array di oggetti

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// Creare array di oggetti per rappresentare i membri del team:		        
+const team = [
+    {
+        fullName: "Wayne Barnett",
+        job: "Founder & CEO",
+        img: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        fullName: "Angela Caroll",
+        job: "Chief Editor",
+        img: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        fullName: "Walter Gordon",
+        job: "Office Manager",
+        img: "walter-gordon-office-manager.jpg",
+    },
+    {
+        fullName: "Angela Lopez",
+        job: "Social Media Manager",
+        img: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        fullName: "Scott Estrada",
+        job: "Developer",
+        img: "scott-estrada-developer.jpg",
+    },
+    {
+        fullName: "Barbara Ramos",
+        job: "Graphic Designer",
+        img: "barbara-ramos-graphic-designer.jpg",
+    },
+];
+console.log(team);
