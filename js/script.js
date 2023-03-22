1bebed14ffc6db60ca3cf110ab2b7f9451ceb462
LucaPertosa <luca.pertosa@hotmail.it>
2023-03-22 18:23:21 +0100

    Inserimento dati dell'array object in DOM

--- js/script.js
+++ js/script.js
@@ -32,3 +32,38 @@ const team = [
     },
 ];
 console.log(team);
+// Prelevo il div dove posizionare la card:
+const myRow = document.querySelector(".row")
+console.log(myRow);
+
+// Creo ciclo for per stampare in console le informazioni di ogni membro del team
+for (let i = 0; i < team.length; i++) {
+    console.log(`Il membro ${i+1} si chiama:` + " " + team[i].fullName);
+    console.log(`Il membro ${i+1} si occupa di:` + " " + team[i].job);
+    console.log(`Foto membro ${i+1}:` + " " + team[i].img);
+    // Genero la card e la inserisco nella row esistente in html
+    const card = generateCard(team[i].fullName, team[i].job, team[i].img)
+    myRow.append(card)
+}
+
+
+
+// FUNCTIONS
+// Creo funzione per creare dinamicamente le col dei singoli membri:
+function generateCard(name, job, text) {
+    const newCard = document.createElement("div");
+    newCard.classList.add("col-12");
+    newCard.innerHTML = 
+    `
+    <div class="card">
+        <div class="card-body">
+            <p class="card-text">${name}</p>
+            <p class="card-text">${job}</p>
+            <p class="card-text">${text}</p>
+        </div>
+    </div>
+    `;
+    return newCard;
+};
+
+console.log(generateCard().innerHTML);
